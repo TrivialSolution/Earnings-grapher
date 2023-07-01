0040c76526ac283ac97dcd74268b2548c2f91a32
Jonathan Chandler <49777173+TrivialSolution@users.noreply.github.com>
2023-07-01 14:52:23 -0700

    Added chart

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var earningsChart;
+var showLines = true;
+
 function fetchEarnings() {
     var ticker = document.getElementById("ticker").value;
 
@@ -19,22 +22,84 @@ function fetchEarnings() {
             var data = JSON.parse(xhr.responseText);
             var earningsData = data.quarterlyEarnings;
 
-            var earningsTable = document.getElementById("earningsBody");
-            earningsTable.innerHTML = "";
+            var dates = [];
+            var actualEarnings = [];
+            var earningsEstimates = [];
 
             for (var i = 0; i < earningsData.length; i++) {
-                var row = earningsTable.insertRow(i);
-                var dateCell = row.insertCell(0);
-                var actualEarningsCell = row.insertCell(1);
-                var earningsEstimateCell = row.insertCell(2);
-
-                dateCell.innerHTML = earningsData[i].fiscalDateEnding;
-                actualEarningsCell.innerHTML = earningsData[i].reportedEPS;
-                earningsEstimateCell.innerHTML = earningsData[i].estimatedEPS;
+                dates.push(earningsData[i].fiscalDateEnding);
+                actualEarnings.push(parseFloat(earningsData[i].reportedEPS));
+                earningsEstimates.push(parseFloat(earningsData[i].estimatedEPS));
             }
+
+            renderChart(dates, actualEarnings, earningsEstimates);
         }
     };
 
     // Send the request
     xhr.send();
+}
+
+function renderChart(dates, actualEarnings, earningsEstimates) {
+    var ctx = document.getElementById("earningsChart").getContext("2d");
+
+    if (earningsChart) {
+        earningsChart.destroy();
+    }
+
+    earningsChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dates,
+            datasets: [{
+                label: 'Actual Earnings',
+                data: actualEarnings,
+                borderColor: 'blue',
+                fill: false,
+                borderWidth: showLines ? 2 : 0
+            }, {
+                label: 'Earnings Estimates',
+                data: earningsEstimates,
+                borderColor: 'green',
+                fill: false,
+                borderWidth: showLines ? 2 : 0
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Quarter End Date'
+                    },
+                    reverse: true
+                },
+                y: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Earnings (in Dollars)'
+                    }
+                }
+            },
+            elements: {
+                line: {
+                    tension: 0, // Remove lines between data points
+                    borderWidth: 2 // Set line width to distinguish data points
+                },
+                point: {
+                    radius: 4, // Set point radius to make data points more visible
+                    hitRadius: 10,
+                    hoverRadius: 5
+                }
+            }
+        }
+    });
+}
+
+function toggleLines() {
+    showLines = !showLines;
+    renderChart(earningsChart.data.labels, earningsChart.data.datasets[0].data, earningsChart.data.datasets[1].data);
 }
